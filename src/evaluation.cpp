107064ea8a233d43fdc7dfeac1bea127905dcb59
0xhtml <34682885+0xhtml@users.noreply.github.com>
2022-02-04 11:45:58 +0100

    Update evaluation

--- src/evaluation.cpp
+++ src/evaluation.cpp
@@ -2,67 +2,57 @@
 
 #include "types.hpp"
 
-const int minorPiecePostionEvaluation[TEAM_COUNT][FIELD_COUNT]{
-    {0, 1, 2, 3, 4, 5, 6, 0,
-     0, 2, 3, 4, 5, 6, 7, 0,
-     0, 2, 3, 4, 5, 6, 7, 0,
-     0, 2, 3, 4, 5, 6, 7, 0,
-     0, 2, 3, 4, 5, 6, 7, 0,
-     0, 2, 3, 4, 5, 6, 7, 0,
-     0, 2, 3, 4, 5, 6, 7, 0,
-     0, 1, 2, 3, 4, 5, 6, 0},
-    {0, 6, 5, 4, 3, 2, 1, 0,
-     0, 7, 6, 5, 4, 3, 2, 0,
-     0, 7, 6, 5, 4, 3, 2, 0,
-     0, 7, 6, 5, 4, 3, 2, 0,
-     0, 7, 6, 5, 4, 3, 2, 0,
-     0, 7, 6, 5, 4, 3, 2, 0,
-     0, 7, 6, 5, 4, 3, 2, 0,
-     0, 6, 5, 4, 3, 2, 1, 0}
+const int pieceSquareTable[PIECE_TYPE_COUNT][FIELD_COUNT / 2]{
+    // HERZMUSCHEL
+    {0,-1,-1, 0, 0, 0, 0, 0,
+     0, 0,-1, 1, 0, 0, 0, 0,
+     0, 3, 3, 2, 3, 0, 0, 0,
+     0, 4, 4, 4, 2, 0, 0, 0},
+    // MOEWE
+    {0,-1, 0, 4, 4, 1, 0, 0,
+     0,-1, 0, 3, 3, 0, 0, 0,
+     0, 0,-1, 1, 1, 0, 0, 0,
+     0, 0,-1, 0, 0, 0, 0, 0},
+    // SEESTERN
+    {0,-1,-1, 0, 0, 1, 0, 0,
+     0,-1,-1, 2, 3, 0, 0, 0,
+     0, 2, 2, 4, 3, 0, 0, 0,
+     0, 4, 4, 3, 2, 0, 0, 0},
+    // ROBBE
+    {0,-1, 0, 1, 0, 0, 0, 0,
+     0, 0, 2, 0, 0, 0, 0, 0,
+     0,-1, 4, 1, 3, 0, 0, 0,
+     0,-1, 4, 2, 1, 0, 0, 0}
 };
 
-const int majorPiecePostionEvaluation[FIELD_COUNT]{
-    -5, -4, -3, -3, -3, -3, -4, -5,
-    -4, -2,  0,  0,  0,  0, -2, -4,
-    -3,  0,  2,  4,  4,  2,  0, -3,
-    -3,  0,  4,  5,  5,  4,  0, -3,
-    -3,  0,  4,  5,  5,  4,  0, -3,
-    -3,  0,  2,  4,  4,  2,  0, -3,
-    -4, -2,  0,  0,  0,  0, -2, -4,
-    -5, -4, -3, -3, -3, -3, -4, -5
-};
+int scoreEvaluation(const GameState &gameState) {
+    return (gameState.score[ONE] - gameState.score[TWO]) * 100;
+}
 
-int evaluatePiecePosition(const GameState &gameState) {
-    int value = 0;
+int pieceSquareEvaluation(int square, const Field &field) {
+    Position pos{square};
+    if (pos.coords.y >= 4) pos.coords.y = 7 - pos.coords.y;
 
-    for (int square = 0; square < FIELD_COUNT; ++square) {
-        const Field &field = gameState.board[square];
-
-        if (!field.occupied) continue;
-
-        int fieldValue;
-
-        if (field.pieceType == ROBBE) {
-            fieldValue = majorPiecePostionEvaluation[square];
-        } else {
-            fieldValue = minorPiecePostionEvaluation[field.team][square];
-        }
-
-        if (field.team == ONE) {
-            value += fieldValue;
-        } else {
-            value -= fieldValue;
-        }
+    if (field.team == ONE) {
+        return pieceSquareTable[field.pieceType][pos.square];
+    } else {
+        pos.coords.x = 7 - pos.coords.x;
+        return -pieceSquareTable[field.pieceType][pos.square];
     }
-
-    return value;
 }
 
 int Evaluation::evaluate(const GameState &gameState) {
     int value = 0;
 
-    value += (gameState.score[ONE] - gameState.score[TWO]) * 55;
-    value += evaluatePiecePosition(gameState);
+    value += (gameState.score[ONE] - gameState.score[TWO]) * 100;
+
+    for (int square = 0; square < FIELD_COUNT; ++square) {
+         const Field &field = gameState.board[square];
+
+         if (!field.occupied) continue;
+
+        value += pieceSquareEvaluation(square, field);
+    }
 
     if (gameState.turn % 2 == TWO) value = -value;
     return value;
