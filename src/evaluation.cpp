5929af2290c9a8cf89dec1685fd428df2b1c58e9
0xhtml <34682885+0xhtml@users.noreply.github.com>
2022-02-11 15:26:27 +0100

    Update evaluation

--- src/evaluation.cpp
+++ src/evaluation.cpp
@@ -4,29 +4,29 @@
 
 const int pieceSquareTable[PIECE_TYPE_COUNT][FIELD_COUNT / 2]{
     // HERZMUSCHEL
-    {0,-1,-1, 0, 0, 0, 0, 0,
-     0, 0,-1, 1, 0, 0, 0, 0,
-     0, 3, 3, 2, 3, 0, 0, 0,
-     0, 4, 4, 4, 2, 0, 0, 0},
+    { 0, 1, 2, 3, 4, 6, 8, 0,
+      0, 1, 2, 3, 4, 6, 7, 0,
+      0, 1, 2, 4, 5, 5, 6, 0,
+      0, 1, 2, 4, 4, 5, 6, 0},
     // MOEWE
-    {0,-1, 0, 4, 4, 1, 0, 0,
-     0,-1, 0, 3, 3, 0, 0, 0,
-     0, 0,-1, 1, 1, 0, 0, 0,
-     0, 0,-1, 0, 0, 0, 0, 0},
+    { 0, 1, 2, 3, 4, 6, 8, 0,
+      0, 1, 2, 3, 4, 6, 7, 0,
+      0, 1, 2, 4, 4, 5, 6, 0,
+      0, 1, 2, 4, 5, 5, 6, 0},
     // SEESTERN
-    {0,-1,-1, 0, 0, 1, 0, 0,
-     0,-1,-1, 2, 3, 0, 0, 0,
-     0, 2, 2, 4, 3, 0, 0, 0,
-     0, 4, 4, 3, 2, 0, 0, 0},
+    { 0, 1, 2, 3, 4, 6, 8, 0,
+      0, 1, 2, 3, 4, 6, 7, 0,
+      0, 1, 2, 4, 5, 5, 6, 0,
+      0, 1, 2, 3, 5, 5, 6, 0},
     // ROBBE
-    {0,-1, 0, 1, 0, 0, 0, 0,
-     0, 0, 2, 0, 0, 0, 0, 0,
-     0,-1, 4, 1, 3, 0, 0, 0,
-     0,-1, 4, 2, 1, 0, 0, 0}
+    { 0, 1, 2, 2, 2, 2, 1, 0,
+      1, 2, 4, 4, 4, 4, 2, 1,
+      2, 4, 6, 6, 6, 6, 4, 2,
+      2, 4, 6, 6, 6, 6, 4, 2}
 };
 
 int scoreEvaluation(const GameState &gameState) {
-    return (gameState.score[ONE] - gameState.score[TWO]) * 100;
+    return (gameState.score[ONE] - gameState.score[TWO]) * 2000;
 }
 
 int pieceSquareEvaluation(int square, const Field &field) {
@@ -41,17 +41,25 @@ int pieceSquareEvaluation(int square, const Field &field) {
     }
 }
 
+int stackEvaluation(const Field &field) {
+    if (field.stacked) {
+        return (field.team == ONE) ? 10 : -10;
+    }
+    return 0;
+}
+
 int Evaluation::evaluate(const GameState &gameState) {
     int value = 0;
 
-    value += (gameState.score[ONE] - gameState.score[TWO]) * 100;
+    value += scoreEvaluation(gameState);
 
     for (int square = 0; square < FIELD_COUNT; ++square) {
-         const Field &field = gameState.board[square];
+        const Field &field = gameState.board[square];
 
-         if (!field.occupied) continue;
+        if (!field.occupied) continue;
 
         value += pieceSquareEvaluation(square, field);
+        value += stackEvaluation(field);
     }
 
     if (gameState.turn % 2 == TWO) value = -value;
